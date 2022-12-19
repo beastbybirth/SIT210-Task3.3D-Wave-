cae849d5e456ca8a79f2c09a1f4970840266708e
beastbybirth <113232311+beastbybirth@users.noreply.github.com>
2022-12-20 03:28:58 +0530

    Update task3.3d.ino

--- task3.3d.ino
+++ task3.3d.ino
@@ -1,15 +1,3 @@
-/*
-  ArduinoMqttClient - WiFi Simple Receive
-
-  This example connects to a MQTT broker and subscribes to a single topic.
-  When a message is received it prints the message to the Serial Monitor.
-
-  The circuit:
-  - Arduino MKR 1000, MKR 1010 or Uno WiFi Rev2 board
-
-  This example code is in the public domain.
-*/
-
 #include <ArduinoMqttClient.h>
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
   #include <WiFiNINA.h>
@@ -22,15 +10,10 @@
 #endif
 
 #include "arduino_secrets.h"
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;    // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
-// To connect with SSL/TLS:
-// 1) Change WiFiClient to WiFiSSLClient.
-// 2) Change port value from 1883 to 8883.
-// 3) Change broker value to a server with a known SSL/TLS root certificate 
-//    flashed in the WiFi module.
+char ssid[] = SECRET_SSID;    
+char pass[] = SECRET_PASS;   
+
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
@@ -42,14 +25,14 @@ const char topic[]  = "SIT210/litwave";
 int n;
 
 void setup() {
-  //Initialize serial and wait for port to open:
+ 
   Serial.begin(9600);
   pinMode(5,OUTPUT);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; 
   }
 
-  // attempt to connect to WiFi network:
+ 
   Serial.print("Attempting to connect to WPA SSID: ");
   Serial.println(ssid);
   while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
@@ -61,12 +44,6 @@ void setup() {
   Serial.println("You're connected to the network");
   Serial.println();
 
-  // You can provide a unique client ID, if not set the library uses Arduino-millis()
-  // Each client must have a unique client ID
-  // mqttClient.setId("clientId");
-
-  // You can provide a username and password for authentication
-  // mqttClient.setUsernamePassword("username", "password");
 
   Serial.print("Attempting to connect to the MQTT broker: ");
   Serial.println(broker);
@@ -85,11 +62,8 @@ void setup() {
   Serial.println(topic);
   Serial.println();
 
-  // subscribe to a topic
   mqttClient.subscribe(topic);
 
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
 
   Serial.print("Waiting for messages on topic: ");
   Serial.println(topic);
@@ -99,14 +73,14 @@ void setup() {
 void loop() {
   int messageSize = mqttClient.parseMessage();
   if (messageSize) {
-    // we received a message, print out the topic and contents
+    
     Serial.print("Received a message with topic '");
     Serial.print(mqttClient.messageTopic());
     Serial.print("', length ");
     Serial.print(messageSize);
     Serial.println(" bytes:");
 
-    // use the Stream interface to print the contents
+   
     while (mqttClient.available()) {
       Serial.print((char)mqttClient.read());
 
